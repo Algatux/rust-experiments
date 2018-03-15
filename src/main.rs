07736b6c560b2c6dc34245d7cba6f12391bdc114
Alessandro Galli <a.galli85@gmail.com>
2018-03-15 21:42:45 +0100

    comparing the inserted number with a random one

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,30 @@
 extern crate rand;
 
 use rand::Rng;
+use std::io;
+use std::cmp::Ordering;
 
 fn main() {
 
     let randomic_number = rand::thread_rng().gen_range(0, 100);
 
     println!("Generated random number: {}", randomic_number);
+
+    println!("Please input your number.");
+
+    let mut string: String = String::new();
+
+    io::stdin().read_line(&mut string)
+        .expect("Failed to read line");
+
+    println!("You inserted: {}", string);
+
+    let string: u32 = string.trim().parse().expect("Please type a number");
+
+    match string.cmp(&randomic_number) {
+        Ordering::Less => println!("It's smaller"),
+        Ordering::Equal => println!("It's the same!"),
+        Ordering::Greater => println!("It's bigger"),
+    }
 
 }
