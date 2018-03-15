7233dd0aa76e40fdb49f7aaf5b293416eea4b1e5
Alessandro Galli <a.galli85@gmail.com>
2018-03-15 20:36:57 +0100

    sum ov cli numeric arguments

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
 
+use std::io::Write;
+use std::str::FromStr;
+
 fn main() {
 
-    let mut array = Vec::new();
+    // type can be inferred
+    let mut numbers: Vec<u64> = Vec::new();
 
-    array.push(8i8);
+    for str in std::env::args().skip(1) {
+        numbers.push(u64::from_str(&str).expect("error parsing argument"));
+    }
 
-    println!("Hello, world! {}", array[0]);
+    if 0 == numbers.len() {
+        writeln!(std::io::stderr(), "No arguments passed").unwrap();
+        std::process::exit(1);
+    }
+
+    let mut total: u64 = 0;
+    for number in numbers {
+        total += number;
+    }
+
+    println!("The sum is {}", total);
 
 }
