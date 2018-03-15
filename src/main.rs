1bd3ea3089e1b20011769b09fe3389999a4c9167
Alessandro Galli <a.galli85@gmail.com>
2018-03-15 21:18:44 +0100

    read from standard input

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,13 @@
 
-extern crate clap;
-use clap::{Arg, App};
+use std::io;
 
 fn main() {
 
-    let matches = App::new("test command")
-        .version("1.0")
-        .author("Alessandro Galli <a.galli85@gmail.com>")
-        .about("Does awesome things")
-        .arg(Arg::with_name("config")
-            .short("c")
-            .long("config")
-            .value_name("FILE")
-            .help("Sets a custom config file")
-            .takes_value(true))
-        .get_matches();
+    let mut string: String = String::new();
 
-    let config = matches.value_of("config").unwrap_or("default.conf");
-    println!("Value for config: {}", config);
+    println!("please enter a string");
 
+    io::stdin().read_line(&mut string).expect("failed to read from stdIn");
+
+    println!("You wrote: {}", string);
 }
